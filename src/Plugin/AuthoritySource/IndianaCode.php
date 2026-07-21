bcf6f384aebc54e673670766f4ba4e36b4caa12c
Jason <jthees@gmail.com>
2026-07-21 14:55:46 -0400

    Add Accept-Encoding header to fix CloudFront returning SPA shell instead of static HTML

--- src/Plugin/AuthoritySource/IndianaCode.php
+++ src/Plugin/AuthoritySource/IndianaCode.php
@@ -67,6 +67,7 @@ class IndianaCode extends AuthoritySourceBase {
     $html = $this->getWebData($url, [
       'headers' => [
         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'Accept-Encoding' => 'gzip, deflate',
       ],
     ]);
 
